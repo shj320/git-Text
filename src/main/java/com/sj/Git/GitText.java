7d3e792fc1f17b3dbdb9f5ae75309d80b829c9c6
shj320 <1257547557@qq.com>
2022-04-04 09:49:08 +0800

    master commit2

--- src/main/java/com/sj/Git/GitText.java
+++ src/main/java/com/sj/Git/GitText.java
@@ -10,5 +10,6 @@ public class GitText {
         System.out.println("hello har");
         System.out.println("hello har3");
         System.out.println("hello har4");
+        System.out.println("hello master");
     }
 }
