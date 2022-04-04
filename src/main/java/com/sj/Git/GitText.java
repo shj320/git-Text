933e77c476a6648a3da2acc67b0515c68e2b6dfa
shj320 <1257547557@qq.com>
2022-04-04 09:37:32 +0800

    second commit

--- src/main/java/com/sj/Git/GitText.java
+++ src/main/java/com/sj/Git/GitText.java
@@ -7,5 +7,6 @@ package com.sj.Git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello har");
     }
 }
